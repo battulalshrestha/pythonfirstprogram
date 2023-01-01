1f4f1d224269b2b59494d337d66a809004d74471
Nishan Shrestha <nishanshrestha@Rajs-MacBook-Pro.local>
2023-01-01 09:57:05 -0600

    secondprogram.py

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # pythonfirstprogram
+
+git init
+git add README.md
+git remote add origin https://github.com/battulalshrestha/master.git
+git branch -M main
+git push -u origin main
+
+https://github.com/battulalshrestha/master.git
+gc
+git add .
+clear
---
+++ secondprogram.py
@@ -0,0 +1,9 @@
+# taking information with the computer 
+name = input("what is your name? :")
+print("My name is " +name)
+add = input("where do you live? :")
+print("I live in " + add)
+clz = input("which college do you read? :")
+print(" I study in " +clz)
+family = input("how many family in your house? :")
+print("I have " + family +" member in my house")
