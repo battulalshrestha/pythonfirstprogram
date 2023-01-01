16146d65ab000cd3d6ac69ea62b748938c648044
Nishan Shrestha <nishanshrestha@Rajs-MacBook-Pro.local>
2023-01-01 10:04:57 -0600

    tkinterfirst.py

---
+++ tkinterfirst.py
@@ -0,0 +1,9 @@
+
+from tkinter import *
+main = Tk()
+myButton = Button(main, text ="click me!")
+myButton1 = Button(main,text ="enter the phonenumber:")
+myButton2 = Button(main,text = "enter your password:")
+myButton1.grid()
+myButton2.grid()
+main.mainloop()
