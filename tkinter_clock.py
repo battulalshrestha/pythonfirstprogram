c630a3053c649959f838e990f66524dd06acf91b
Nishan Shrestha <nishanshrestha@Rajs-MBP.domain.name>
2023-01-02 04:51:52 -0600

    clock program on tkinter

---
+++ tkinter_clock.py
@@ -0,0 +1,35 @@
+
+# importing whole module
+
+from tkinter import *
+ 
+# importing strftime function to
+# retrieve system's time
+from time import strftime
+ 
+# creating tkinter window
+main = Tk()
+main.title('digital clock')
+ 
+# This function is used to
+# display time on the Button 
+ 
+def time():
+    string = strftime('%H:%M:%S %p')
+    Button.config(text=string)
+    Button.after(1000, time)
+ 
+ 
+# Styling the label widget so that clock
+# will look more attractive
+myButton= Button(main, font=('calibri', 100, 'bold'),
+            background='purple',
+            foreground='white')
+myButton.grid()
+ 
+# Placing clock at the centre
+# of the tkinter window
+myButton=Button(main,anchor=NW)
+myButton.grid()
+ 
+main.mainloop()
